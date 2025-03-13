e7c859f9111dc9c0116d0566b2961cf35e3eaddc
Yury Brazgunou <jurabrezgunov@gmail.com>
2025-03-13 13:36:11 +0300

    renamed, database added

--- src/main/java/com/quizmaker/MainController.java
+++ src/main/java/com/quizmaker/MainController.java
@@ -1,4 +1,4 @@
-package com.quizapplication;
+package com.quizmaker;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/quizmaker/QuizApplication.java
+++ src/main/java/com/quizmaker/QuizApplication.java
@@ -1,4 +1,4 @@
-package com.quizapplication;
+package com.quizmaker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
